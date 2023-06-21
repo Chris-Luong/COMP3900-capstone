46548f33d08fd8d928fe84a3e8f344c3556270e7
grace-12345 <135817463+grace-12345@users.noreply.github.com>
2023-06-21 09:21:17 +0000

    add in employee url

--- backend/app.js
+++ backend/app.js
@@ -39,8 +39,8 @@ function authorize(role) {
   }
 
   
-app.post('/login', (req, res) => {
-    const { username, password, role} = req.body;
+app.post('/login/customers', (req, res) => {
+    const { username, password } = req.body;
 
     const query = 'SELECT * FROM accounts WHERE username = ? AND password = ? AND role = ?';
     db.query(query, [username, password], (err, results) => {
@@ -60,6 +60,27 @@ app.post('/login', (req, res) => {
           role: user.role 
         });
         return res.render('admin/index', { msg: "Login Successfully", err: "" });
+      }
+    });
+  });
+
+app.post('/login/employees', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = 'SELECT * FROM accounts WHERE username = ? AND password = ?';
+    db.query(query, [username, password], (err, results) => {
+      if (err) { 
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+  
+      if (results.length === 0) { 
+        res.status(401).json({ error: 'Invalid username or password' });
+      } else {
+
+        const user = results[0];
+        const role = user.role;
+        return res.render('admin/index', { msg: "Login Successfully", err: "" , data: role });
       }
     });
   });
