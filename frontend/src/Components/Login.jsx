3b33610fc81cdf1353b960a33b21a30d65a144ed
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2023-06-22 23:10:07 +1000

    created login form based off register, have not split it for staff and customer yet

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,14 +1,92 @@
-import { Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import LoginContext from "./Context/login-context";
+import styled from "@emotion/styled";
+
+// TOOD: match schema with acceptance criteria
+const schema = Yup.object().shape({
+  email: Yup.string().required("Email is required").email("Email is invalid"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters")
+    .max(40, "Password must not exceed 40 characters"),
+});
+
+const CenterCard = styled("Card")({
+  display: "flex",
+  textAlign: "left",
+  maxHeight: "700px",
+  maxWidth: "50vh",
+  margin: "auto",
+  marginTop: "15vh",
+  border: "1px solid #d7d7d7",
+  padding: "1rem",
+  borderRadius: "6px",
+});
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const login = useContext(LoginContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email, password);
+    localStorage.setItem("auth", true);
+    localStorage.setItem("user-email", event.email);
+    login.setIsLoggedIn(true);
+  }
+
   return (
-    <div>
-      <p>Welcome back</p>
-      <Typography align='center' variant='overline'>
-        Don't have an account? <Link to='../register'>Register here</Link>
-      </Typography>
-    </div>
+    <Formik
+      validationSchema={schema}
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+    >
+      <>
+        <form onSubmit={handleSubmit}>
+          <CenterCard>
+            <Stack spacing={2} direction='column' width='100%'>
+              <Typography align='center' variant='h3'>
+                Welcome back
+              </Typography>
+              <TextField
+                type='email'
+                variant='outlined'
+                color='secondary'
+                label='Email'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                sx={{ mb: 4 }}
+              />
+              <TextField
+                type='password'
+                variant='outlined'
+                color='secondary'
+                label='Password'
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                sx={{ mb: 4 }}
+              />
+              <Button variant='outlined' color='secondary' type='submit'>
+                Login
+              </Button>
+              <Typography align='center' variant='overline'>
+                Don't have an account?{" "}
+                <Link to='../register'>Register here</Link>
+              </Typography>
+            </Stack>
+          </CenterCard>
+        </form>
+      </>
+    </Formik>
   );
 };
 
