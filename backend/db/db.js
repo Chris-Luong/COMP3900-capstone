a34f0891b3e7563475f5435090bf703fb94c7506
Rodney <rodney-boyce@hotmail.com>
2023-06-24 21:46:26 +1000

    Added registration, refactored db code and init, refactored auth routes

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,18 +1,30 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
-const DB_NAME = "queuequicker"
+const INIT_DB = fs.readFileSync('init_db.sql', 'utf8').split(/;\r?\n/);
 
+// Create connection to MySQL
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"password",
-    database: DB_NAME,
+    password:"password"
 })
 
-
+// Connect
 db.connect((err) => {
     if (err) console.error(err.message);
-    else console.log("database connected")
+    else console.log("Database connected.")
 });
+
+// Create and initialise the database
+for (let query of INIT_DB) {
+    db.query(query.trim().replace(';', ''), (err, results) => {
+        if (err) {
+          console.error('Error initialising database:', err);
+          return;
+        }
+      });
+}
+console.log('Database initialised.');
 
 module.exports = db;
