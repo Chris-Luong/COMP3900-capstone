9249b878aef244a1c06349f947df007015dbe434
Rodney <rodney-boyce@hotmail.com>
2023-06-24 22:26:15 +1000

    modified accounts table cols to not null

--- backend/init_db.sql
+++ backend/init_db.sql
@@ -4,9 +4,9 @@ USE queuequicker;
 
 CREATE TABLE IF NOT EXISTS account (
     accountId BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    firstname VARCHAR(50) NULL,
-    lastname VARCHAR(50) NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('kitchen stuff', 'manager', 'customer', 'wait stuff')
+    role ENUM('Customer', 'Wait Staff', 'Kitchen Staff', 'Manager')
 );
