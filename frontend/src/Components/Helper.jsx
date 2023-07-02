059ca0e9a0dc821ab5be5e7f781fb5cc1453f7b8
Alan932 <47587128+Alan932@users.noreply.github.com>
2023-07-02 21:47:56 +1000

    remove unnecessary comments and console logs

--- frontend/src/Components/Helper.jsx
+++ frontend/src/Components/Helper.jsx
@@ -6,18 +6,9 @@ export const getAllMenuItems = async () => {
   try {
     const res = await sendRequest('/menu', 'GET');
     const items = [];
-    // console.log(res.items.length);
-    // res.items.forEach((item) => {
-    //   // console.log(item);
-    //   items.push(item);
-    // })
-    // items.push(res.items);
-    // console.log(res.items);
     for (const item in res.items) {
-      // console.log(item);
       items.push(res.items[item]);
     }
-    console.log(items);
     return items;
   } catch (err) {
     console.log(err);
