c14ee0d00ca8409542e84fdd2cd0a01d9d4afad5
GungeetTank <135817394+GungeetTank@users.noreply.github.com>
2023-07-08 12:57:13 +1000

    improving landing page for website

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -1,28 +1,45 @@
-import { Link } from "react-router-dom";
-import LoginContext from "./Context/login-context";
+import { Link as RouterLink } from "react-router-dom";
 import { useContext } from "react";
+import { Typography, Button, Box, Container } from "@mui/material";
+import LoginContext from "./Context/login-context";
 
 const Landing = () => {
   const login = useContext(LoginContext);
 
   return (
-    <div>
-      Landing page here
-      {!login.isLoggedIn && (
-        <>
-          <p>
-            Already have an account? <Link to="../login">Login here</Link>
-          </p>
-          <p>
-            Don't have an account? <Link to="../register">Register here</Link>
-          </p>
-          <p>
-            Inside our restaurant?{" "}
-            <Link to="../restaurant">Check in and order here</Link>
-          </p>
-        </>
-      )}
-    </div>
+    <Container>
+      <Box sx={{ textAlign: 'center', padding: 3 }}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to Our Restaurant Wait Management System
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Easily manage your waiting time, check our menu, and enjoy a seamless dining experience.
+        </Typography>
+
+        <Box sx={{ mt: 4 }}>
+          <Button variant="contained" color="primary" component={RouterLink} to="../pricing" sx={{ mr: 2 }}>
+            See Pricing
+          </Button>
+          <Button variant="contained" color="secondary" component={RouterLink} to="../menus">
+            Check Our Menus
+          </Button>
+        </Box>
+
+        {!login.isLoggedIn && (
+          <Box sx={{ mt: 5 }}>
+            <Typography variant="body1" gutterBottom>
+              Already have an account? <RouterLink to="../login">Login here</RouterLink>
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Don't have an account? <RouterLink to="../register">Register here</RouterLink>
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Inside our restaurant? <RouterLink to="../restaurant">Check in and order here</RouterLink>
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Container>
   );
 };
 
--- frontend/src/Components/Restaurant/Menu.jsx
+++ frontend/src/Components/Restaurant/Menu.jsx
@@ -2,47 +2,46 @@ import { useState, useEffect } from "react";
 import {
   Card,
   CardHeader,
+  CardMedia,
   CardContent,
   CardActions,
   Typography,
   Button,
   CircularProgress,
+  Box
 } from "@mui/material";
 import { getAllMenuItems } from "../Helper";
 
-const MenuItemCard = ({ name, description, price, availability }) => {
-  // TODO: add item image
+const MenuItemCard = ({ name, description, price, availability, imageUrl }) => {
   return (
-    <>
-      {availability === 1 && (
-        <Card variant="outlined" sx={{ maxWidth: "30vw", margin: "auto" }}>
-          <CardHeader title={name} />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {description}
-            </Typography>
-            <Typography>{price}</Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Add to Order</Button>
-          </CardActions>
-        </Card>
-      )}
-      {availability === 0 && (
-        <Card>
-          <CardHeader title={name} />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {description}
-            </Typography>
-            <Typography>{price}</Typography>
-          </CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Not available
+    <Box sx={{ mb: 3 }}>
+      <Card variant="outlined" sx={{ maxWidth: "30vw", margin: "auto" }}>
+        <CardHeader title={name} />
+        {imageUrl && (
+          <CardMedia
+            component="img"
+            height="140"
+            image={imageUrl}
+            alt={name}
+          />
+        )}
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {description}
           </Typography>
-        </Card>
-      )}
-    </>
+          <Typography variant="h6">{`$${price}`}</Typography>
+        </CardContent>
+        <CardActions>
+          {availability === 1 ? (
+            <Button size="small">Add to Order</Button>
+          ) : (
+            <Typography sx={{ fontSize: 14 }} color="error" gutterBottom>
+              Not available
+            </Typography>
+          )}
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
@@ -55,23 +54,44 @@ const Menu = () => {
     const getMenuItems = async () => {
       let itemsData = await getAllMenuItems();
       setMenuItems(itemsData);
-      console.log(itemsData);
       setLoading(false);
     };
     getMenuItems();
   }, []);
 
+  const demoItems = [
+    {
+      id: "demo1",
+      name: "Demo Item 1",
+      description: "This is a demo item.",
+      price: "9.99",
+      availability: 1,
+      imageUrl: "/path/to/image1.jpg"
+    },
+    {
+      id: "demo2",
+      name: "Demo Item 2",
+      description: "This is another demo item.",
+      price: "14.99",
+      availability: 0,
+      imageUrl: "/path/to/image2.jpg"
+    },
+  ];
+
+  const allItems = [...menuItems, ...demoItems];
+
   return (
     <>
       {loading && <CircularProgress />}
       {!loading &&
-        menuItems.map((item) => (
+        allItems.map((item) => (
           <MenuItemCard
             key={item.id}
             name={item.name}
             description={item.description}
             price={item.price}
             availability={item.availability}
+            imageUrl={item.imageUrl}
           />
         ))}
     </>
