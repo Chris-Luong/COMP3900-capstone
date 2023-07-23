018549b8909294ec1602359fe099e3c611946145
GraceLi1314 <ligrace128@gmail.com>
2023-07-24 08:06:01 +1000

    added tableCapacity

--- backend/db/queries/booking.queries.js
+++ backend/db/queries/booking.queries.js
@@ -1,8 +1,9 @@
 const findBooking = 
 `SELECT t.id as tableId, t.tableName as tableName, t.capacity as capacity
 FROM tables t
-LEFT JOIN bookings b ON t.id = b.table_id AND b.date = ? AND ((b.start_time <= ? AND b.end_time > ?) 
-OR(b.start_time >= ? AND b.start_time < ?))
+LEFT JOIN bookings b ON t.id = b.table_id AND b.date = ? 
+AND ((b.start_time <= ? AND b.end_time > ?) 
+    OR(b.start_time >= ? AND b.start_time < ?))
 WHERE t.capacity >= ? AND b.id IS NULL;`
 
 const findBookingByAccountId = `
@@ -14,7 +15,7 @@ const createBookingByTableId = `INSERT INTO bookings(user_id, table_id, date, st
 VALUES (?, ?, ?, ?, ?, ?);`
 
 const viewBookingsByDate =  `
-SELECT b.id as bookId, a.lastname AS userName, t.id as tableId, t.tableName as tableName, b.start_time as bookingStart, b.end_time as bookingEnd, b.guests as guests
+SELECT b.id as bookId, a.lastname AS userName, t.id as tableId, t.capacity as tableCapacity, b.start_time as bookingStart, b.end_time as bookingEnd, b.guests as guests
 FROM bookings b
 JOIN account a ON b.user_id = a.accountId
 JOIN tables t ON b.table_id = t.id
