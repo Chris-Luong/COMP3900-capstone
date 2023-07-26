cd365739aed5cee17042cd031f3dff1474ab645a
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2023-07-27 01:39:32 +1000

    formatting time on reservation dashboard cards

--- frontend/src/Components/UI/ReservationDashboard.jsx
+++ frontend/src/Components/UI/ReservationDashboard.jsx
@@ -3,6 +3,7 @@ import { getReservations } from "../Helper";
 import { useState, useEffect } from "react";
 
 import {
+  Button,
   Card,
   CardContent,
   CardHeader,
@@ -26,23 +27,64 @@ const ReservationDashboard = () => {
   const date = "2023-07-22";
   console.log(`date is ${date}`);
 
-  // TODO: this doesn't seem to run
   useEffect(() => {
     // setLoading(true);
     const retrieveReservations = async () => {
-      // NOTE: if returnds object, could use JSON.stringify from chatGPT suggestion
       const bookingData = await getReservations("", date);
-      console.log(`bookingdata is ${JSON.stringify(bookingData)}`);
+      // console.log(`bookingdata is ${JSON.stringify(bookingData)}`);
       setBookings(bookingData);
       // setLoading(false);
     };
     retrieveReservations();
   }, []);
 
-  console.log(bookings);
-  bookings.map((booking) =>
-    console.log(`each booking Id is ${booking.bookId}`)
-  );
+  // TODO: get formmatted dates to be correct, then display
+  // TODO: implement notification banner instead of alerts
+  const bookingCards = () => {
+    const dates = bookings.map((booking) => {
+      const formattedStart = dayjs(booking.bookingStart).format("h:mm A");
+      const formattedEnd = dayjs(booking.bookingEnd).format("h:mm A");
+      console.log(formattedStart, formattedEnd);
+    });
+    return bookings.map((booking) => (
+      <Grid item xs={12} sm={4} md={3} key={booking.bookId}>
+        <Card
+          sx={{
+            width: "100%",
+            height: "100%",
+            margin: "10px",
+            border: 1,
+            borderColor: "rgba(216, 206, 222, 0.8)",
+            transition: "all 0.3s ease-out",
+            boxShadow: "0 14px 26px rgba(0, 0, 0, 0.04)",
+            "&:hover": {
+              transform: "translateY(-5px) scale(1.005) translateZ(0)",
+              boxShadow: "0 12px 24px rgba(156, 39, 176, 0.5)",
+            },
+          }}
+        >
+          <CardHeader
+            title={booking.userName}
+            subheader={`${booking.bookingStart} - ${booking.bookingEnd}`}
+          />
+          <Divider />
+          <CardContent>
+            <Typography variant='body2' color='textSecondary'>
+              Table: {booking.tableId}
+            </Typography>
+            <Typography variant='body2' color='textSecondary'>
+              Party Size: {booking.guests}
+            </Typography>
+            <Typography variant='body2' color='textSecondary'>
+              Table capacity: {booking.tableCapacity}
+            </Typography>
+          </CardContent>
+          <Button>Confirm Customer Arrival</Button>
+        </Card>
+      </Grid>
+    ));
+  };
+
   return (
     // <>
     //   {loading && <CircularProgress />}
@@ -67,43 +109,7 @@ const ReservationDashboard = () => {
         Today's Reservations
       </Typography>
       <Grid container spacing={2}>
-        {bookings.map((booking) => (
-          <Grid item xs={12} sm={4} md={3} key={booking.bookId}>
-            <Card
-              sx={{
-                width: "100%",
-                height: "100%",
-                margin: "10px",
-                cursor: "pointer",
-                border: 1,
-                borderColor: "rgba(216, 206, 222, 0.8)",
-                transition: "all 0.3s ease-out",
-                boxShadow: "0 14px 26px rgba(0, 0, 0, 0.04)",
-                "&:hover": {
-                  transform: "translateY(-5px) scale(1.005) translateZ(0)",
-                  boxShadow: "0 12px 24px rgba(156, 39, 176, 0.5)",
-                },
-              }}
-            >
-              <CardHeader
-                title={booking.userName}
-                subheader={`${booking.bookingStart} - ${booking.bookingEnd}`}
-              />
-              <Divider />
-              <CardContent>
-                <Typography variant='body2' color='textSecondary'>
-                  Table: {booking.tableId}
-                </Typography>
-                <Typography variant='body2' color='textSecondary'>
-                  Party Size: {booking.guests}
-                </Typography>
-                <Typography variant='body2' color='textSecondary'>
-                  Table capacity: {booking.tableCapacity}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+        {bookingCards()}
       </Grid>
     </Paper>
     //   )}
