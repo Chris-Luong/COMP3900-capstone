a7e6561523690b8dac5ed87c7348d70dda6884b2
Rodney <rodney-boyce@hotmail.com>
2023-07-30 16:27:25 +1000

    semi working

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,17 +2,19 @@ const mysql = require("mysql");
 const fs = require("fs");
 
 const INIT_DB = fs.readFileSync("init_db.sql", "utf8").split(/;\r?\n/);
+const HOST = "127.0.0.1"
+//const HOST = "mysql"
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-  host: "127.0.0.1",
+  host: HOST,
   user: "root",
   password: "password",
   port: "3306",
 });
 
 // Connect
-db.connect((err) => {
+  db.connect((err) => {
   if (err) console.error(err.message);
   else console.log("Database connected.");
 });
