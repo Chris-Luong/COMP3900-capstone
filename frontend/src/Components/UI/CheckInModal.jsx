6caf6037d9ee4d1d1598347225217dccc3116ceb
GungeetTank <135817394+GungeetTank@users.noreply.github.com>
2023-08-04 16:32:41 +1000

    adding comments to modals

--- frontend/src/Components/UI/CheckInModal.jsx
+++ frontend/src/Components/UI/CheckInModal.jsx
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
+// Styles for the modal content
 const style = {
   position: "absolute",
   top: "50%",
@@ -21,16 +22,19 @@ const style = {
   borderRadius: "15px",
 };
 
+// CheckInModal component that displays the guest check-in form
 const CheckInModal = ({
   showCheckInModal,
-  toggleCheckInModal,
-  guestNumber,
-  handleGuestNumberChange,
-  handleCheckIn,
+  toggleCheckInModal, 
+  guestNumber, 
+  handleGuestNumberChange, 
+  handleCheckIn, 
 }) => {
   return (
     <Modal open={showCheckInModal}>
+      {/* Card containing the modal content */}
       <Card sx={style}>
+        {/* Modal title and close button */}
         <DialogTitle sx={{ px: "24px" }}>
           <Typography variant="h4">Guest Check In</Typography>
           <IconButton
@@ -45,7 +49,10 @@ const CheckInModal = ({
             <CloseIcon />
           </IconButton>
         </DialogTitle>
+
+        {/* Modal content area */}
         <DialogContent dividers>
+          {/* Input field for entering the number of guests */}
           <TextField
             label="Number of guests"
             name="guests"
@@ -56,7 +63,10 @@ const CheckInModal = ({
             fullWidth
           />
         </DialogContent>
+
+        {/* Modal actions */}
         <DialogActions sx={{ padding: "10px" }}>
+          {/* Check-in button */}
           <Button
             variant="contained"
             sx={{ backgroundColor: "green" }}
@@ -64,6 +74,8 @@ const CheckInModal = ({
           >
             Check In
           </Button>
+
+          {/* Close button */}
           <Button
             variant="contained"
             sx={{ backgroundColor: "grey" }}
