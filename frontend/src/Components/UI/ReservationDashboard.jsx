14fb67104bcae036a28d9bc4ef142e3899c8026a
GungeetTank <135817394+GungeetTank@users.noreply.github.com>
2023-08-04 16:25:52 +1000

    customer dashboard card should show bookingID

--- frontend/src/Components/UI/ReservationDashboard.jsx
+++ frontend/src/Components/UI/ReservationDashboard.jsx
@@ -103,6 +103,9 @@ const ReservationDashboard = (props) => {
                 Table: {booking.tableId}
               </Typography>
               <Typography variant="body2" color="textSecondary" gutterBottom>
+                Table: {booking.bookingId}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" gutterBottom>
                 Party Size: {booking.guests}
               </Typography>
               <Typography variant="body2" color="textSecondary" gutterBottom>
